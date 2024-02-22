24bf393e080e7b6290ae9bc917bbe76151e1ff04
meow2704 <146491474+meow2704@users.noreply.github.com>
2024-02-22 13:30:29 -0800

    Update pattern.py

--- labpractice/pattern.py
+++ labpractice/pattern.py
@@ -1,9 +1,28 @@
- # This program displays a triangle pattern.
-BASE_SIZE = 8
+print('Triangle') #Alex Raduenz Nested Loops and Input Validation Practice
+for a in range(5, 0, -1):
+	for b in range(a):
+		print('*', end=' ')
+	print()
+print('Pyramid')
+for c in range(5):
+	for d in range(5-c-1):
+		print(' ', end='')
+	for e in range(2*c+1):
+		print('*', end='')
+	print()
 
-for r in range(BASE_SIZE,1,-1):
-	for c in range(- r):
-		print(" ", end=' ')
-	for c in range(r-1):
-		print(' ', end='*')
-	print( )
+#Extra Credit
+print('Diamond')
+for f in range(5):
+	for g in range(5-f-1):
+		print(' ', end='')
+	for h in range(2*f+1):
+		print('*', end='')
+	print()
+
+for f in range(4, 0, -1):
+	for g in range(5-f):
+		print(' ', end='')
+	for h in range(2*f-1):
+		print('*', end='')
+	print()
